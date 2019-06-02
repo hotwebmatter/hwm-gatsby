0224c7289c16fe0e4e4e642094c91d5f253dde9d
Matthew Obert <github@hotwebmatter.com>
2019-06-02 12:59:42 -0400

    Gatsby tutorial - Link

---
+++ hello-world/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+	<Link to="/">Home</Link>
+    <Header headerText="About Gatsby" />
+    <Header headerText="It's pretty cool" />
+    <p>Such wow. Very React.</p>
+  </div>
+)
